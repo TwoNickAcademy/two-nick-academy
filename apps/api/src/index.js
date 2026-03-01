3d23cdbd132578aa6bfadcfd67f4d47ea28f03b3
Angel Estrada <g.tepgarcis@gmail.com>
2026-03-01 03:49:00 -0600

    feat: flujo restaurado y limpio

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 API de TwoNick Academy funcionando!');
+  res.send('🚀 API de TwoNick Academy funcionando al mil porciento banda!');
 });
 
 app.listen(port, () => {
